6e49145ed64ed9237f593f92c082d72ece805894
RebornKang <reborn_kang@163.com>
2022-01-07 19:39:37 +0800

    add dym handle code

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Grabber.h"
+#include "DrawDebugHelpers.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -19,7 +22,19 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("hahaha"));
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting hahaha"));
+
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle)
+	{
+		// Physics handle is found
+		UE_LOG(LogTemp, Warning, TEXT("found the handle : %s"), *PhysicsHandle->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s handle missing"), *GetOwner()->GetName());
+	}
+
 	// ...
 	
 }
@@ -29,6 +44,54 @@ void UGrabber::BeginPlay()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+	// // TODO log out to test
+	
+	// UE_LOG(LogTemp, Warning, TEXT("test"));
+	/*UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"),
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotation.ToString());*/
+
+	FVector LineTraceEnd = PlayerViewPointLocation +PlayerViewPointRotation.Vector() * Reach;
+
+	// Draw a red trace in the world
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
+	// Setup query parameter
+
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	//UE_LOG(LogTemp, Warning, TEXT(PlayerViewPointRotation.ToString());
+	// Get Player view point this tick
+	// Ray-cast out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+
+		);
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+	UE_LOG(LogTemp, Warning, TEXT("you will pick: %s"), *(ActorHit->GetName()));
+	}
+	// See what we hit
 
 	// ...
 }
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,8 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UGrabber : public UActorComponent
@@ -24,5 +24,11 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	// How far ahead of the player can we reach in cm
+	float Reach = 100.f;
+	
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	
 		
 };
